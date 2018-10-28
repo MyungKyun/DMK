c8f8640016bf32a324077082747b15f40314a5b9
Mk <ultraman232@naver.com>
2018-10-28 20:11:27 +0900

    생성자 및 IoProcessor 가상함수 오버라이드 추가

--- FrameWork/SendProcessor.cpp
+++ FrameWork/SendProcessor.cpp
@@ -20,3 +20,8 @@ Void	SendProcessor::Send()
 	// 보낼수 있을때 정해둔 크기만큼 보낸다.
 	// 여유가 되면 바로 보낸다.
 }
+
+Void SendProcessor::CompleteIoEventProcess(Overlapped_Ex * overlapped, Int numberOfTransferredBytes, Bool ioEventResult)
+{
+	
+}
--- FrameWork/SendProcessor.h
+++ FrameWork/SendProcessor.h
@@ -1,1 +1,15 @@
 #pragma once
+
+class SendProcessor : public IoProcessor
+{
+
+
+public:
+
+	SendProcessor();
+	~SendProcessor();
+
+	
+	Void	Send();
+	Void	CompleteIoEventProcess(Overlapped_Ex* overlapped, Int numberOfTransferredBytes, Bool ioEventResult) override;
+};
