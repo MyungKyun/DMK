a9c714a59c7c321d1f766ca9db7614aac785c9d2
Mk <ultraman232@naver.com>
2018-10-30 17:13:42 +0900

    shared_mutex 사용하게 수정 기존의 Lock 클래스는 주석처리.

--- FrameWork/Lock.h
+++ FrameWork/Lock.h
@@ -1,31 +1,31 @@
 #pragma once
 
-
-class Lock
-{
-private:
-	Lock_t  mtx_;
-	DLong	lockId_;
-	Int		threadId_;
-
-	WString		LockfileName_;
-	Int			LockLine_;
-
-public:
-
-	Lock();
-	~Lock();
-	
-	Void DoLock(LPCWSTR fileName, Int lineNo);
-	Void DoUnlock();
-	
-	Void SetThreadId(Int id);
-
-
-	UDLong GetLockId() const;
-	Int GetThreadId() const;
-	Lock_t& GetMutex();	
-};
+//
+//class Lock
+//{
+//private:
+//	Lock_t  mtx_;
+//	DLong	lockId_;
+//	Int		threadId_;
+//
+//	WString		LockfileName_;
+//	Int			LockLine_;
+//
+//public:
+//
+//	Lock();
+//	~Lock();
+//	
+//	Void DoLock(LPCWSTR fileName, Int lineNo);
+//	Void DoUnlock();
+//	
+//	Void SetThreadId(Int id);
+//
+//
+//	UDLong GetLockId() const;
+//	Int GetThreadId() const;
+//	Lock_t& GetMutex();	
+//};
 
 
 #define		DECLARE_LOCK		std::shared_mutex	lock
