4ba65180e56cf91f4c91564cb76fbddebe0e6414
Mk <ultraman232@naver.com>
2018-11-04 15:34:19 +0900

    우선 Send할 데이터를 큐잉 하여 보내도록하자, 큐잉을 하지 않고 보내는 방법을 생각해보자

--- FrameWork/SendBufferQueue.cpp
+++ FrameWork/SendBufferQueue.cpp
@@ -15,10 +15,20 @@ SendBufferQueue::~SendBufferQueue()
 	}
 }
 
-Void	SendBufferQueue::Push(std::shared_ptr<SendBuffer> sendBuffer, Bool& sendImmediately)
+Bool		SendBufferQueue::Empty()
 {
-	sendQue_.push(sendBuffer);
+	return sendQue_.empty();
+}
+
+Void	SendBufferQueue::Push(Byte* buf, Int len, Bool& sendImmediately)
+{
 	currentCount_.fetch_add(1);
+
+	std::shared_ptr<SendBuffer> sendBuffer = std::make_shared<SendBuffer>();
+	sendBuffer->buffer_ = buf;
+	sendBuffer->len_ = len;
+
+	sendQue_.push(sendBuffer);
 
 	if (1 == currentCount_)
 	{
@@ -26,11 +36,62 @@ Void	SendBufferQueue::Push(std::shared_ptr<SendBuffer> sendBuffer, Bool& sendImm
 	}
 }
 
-
-Void	SendBufferQueue::Pop(std::shared_ptr<SendBuffer> out)
+Void		SendBufferQueue::Copy(Byte* buf, Int bufferSize, Int& outSize)
 {
-	if (true == sendQue_.try_pop(out))
+	outSize = 0;
+	Int remainBufSize = bufferSize;
+	if (0 >= remainBufSize)
 	{
-		currentCount_.fetch_sub(1);
+		assert(false);
+	}
+
+	auto first = reservingTo_.begin();
+	auto last = reservingTo_.end();
+
+	for (; first != last; ++first)
+	{
+		auto bufPtr = *first;
+		
+		if (remainBufSize < bufPtr->len_)
+		{
+			reservingTo_.erase(std::begin(reservingTo_), first);
+			return;
+		}
+
+		::memcpy_s(buf, remainBufSize, bufPtr->buffer_, bufPtr->len_);
+
+		outSize += bufPtr->len_;
+		remainBufSize -= bufPtr->len_;
+	}
+	
+	pop(buf + outSize, remainBufSize, outSize);
+}
+
+
+Void	SendBufferQueue::pop(Byte* buf,Int bufferSize, Int& outSize)
+{
+	Int remainSize = bufferSize;
+
+	while (false == sendQue_.empty())
+	{
+		std::shared_ptr<SendBuffer> sendBuffer;
+		if (true == sendQue_.try_pop(sendBuffer))
+		{
+			currentCount_.fetch_sub(1);
+		}
+
+		if (0 >= remainSize || 
+			remainSize < sendBuffer->len_)
+		{
+			reservingTo_.push_back(std::move(sendBuffer));
+		}
+		else
+		{
+			::memcpy_s(buf, remainSize, sendBuffer->buffer_, sendBuffer->len_);
+
+			outSize += sendBuffer->len_;
+			remainSize -= outSize;
+
+		}
 	}
 }
--- FrameWork/SendBufferQueue.h
+++ FrameWork/SendBufferQueue.h
@@ -10,7 +10,7 @@ struct SendBuffer
 	Byte*		buffer_;
 	Int			len_;
 
-	SendBuffer(Int packetSize)
+	explicit SendBuffer(Int packetSize = PACKET_MAX_SIZE)
 		: len_(packetSize)
 	{
 		assert(packetSize <= PACKET_MAX_SIZE);
@@ -19,13 +19,8 @@ struct SendBuffer
 
 	~SendBuffer()
 	{
-
 	}
 
-	Void	SetData(Byte* buffer)
-	{
-		::memcpy_s(buffer_, len_, buffer, len_);
-	}
 };
 
 
@@ -33,12 +28,17 @@ class SendBufferQueue
 {
 	concurrency::concurrent_queue<std::shared_ptr<SendBuffer>>	sendQue_;
 	std::atomic_int												currentCount_;
+	std::vector<std::shared_ptr<SendBuffer>>					reservingTo_;
 
 public:
 
 	SendBufferQueue();
 	~SendBufferQueue();
 	
-	Void		Push(std::shared_ptr<SendBuffer> sendBuffer, Bool& sendImmediately);
-	Void		Pop(std::shared_ptr<SendBuffer> out);
+	Void		Push(Byte* buf, Int len, Bool& sendImmediately);
+	Void		Copy(Byte* buf, Int buffersize, Int& outSize);
+	Bool		Empty();
+private:
+
+	Void		pop(Byte* buf, Int bufferSize, Int& outSize);
 };
