fe0cb9dd5633efb9ee150e2e6330ad3a231490f1
Mk <ultraman232@naver.com>
2018-11-25 17:12:28 +0900

    spdlog 라이브러리 사용하여 Log 클래스 작성

---
+++ FrameWork/AsyncLogMsg.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/details/log_msg.h"
+
+AsyncLogMsg::AsyncLogMsg(const spdlog::details::log_msg& lmsg)
+	: logLevel_(lmsg.level)
+	, time_(lmsg.time)
+	, fileName_(lmsg.source.filename)
+	, fileLine_(lmsg.source.line)
+	, msgId_(lmsg.msg_id)
+	, msgType_(AsyncMsgType::LOG)
+	, threadId_(static_cast<DWORD>(lmsg.thread_id))
+	, text_(lmsg.payload.begin(),lmsg.payload.end())
+{
+
+}
+
+
+AsyncLogMsg::AsyncLogMsg(AsyncMsgType msgType)
+	: logLevel_(spdlog::level::info)	
+	, threadId_(0)
+	, msgId_(0)
+{
+
+}
+
+Void AsyncLogMsg::FillupLogMsg(spdlog::details::log_msg& msg, String* loggerName)
+{
+	msg.logger_name = loggerName;
+	msg.time = time_;
+	msg.thread_id = threadId_;
+	msg.level = logLevel_;
+	const_cast<spdlog::string_view_t&>(msg.payload) = text_;
+	msg.msg_id = msgId_;
+	msg.source.filename = fileName_.c_str();
+	msg.source.line = fileLine_;
+}
+
+
---
+++ FrameWork/Log.cpp
@@ -0,0 +1,126 @@
+#include "stdafx.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include <filesystem>
+
+AsyncLog::AsyncLog(const std::string& loggerName, spdlog::sinks_init_list sinksInitList)
+	: spdlog::logger(loggerName, sinksInitList.begin(), sinksInitList.end())
+{
+
+}
+
+
+Void AsyncLog::flush_()
+{
+	AsyncLogMsg lmsg;
+	while (msgQue_.try_pop(lmsg))
+	{
+		spdlog::details::log_msg  copymsg;
+		lmsg.FillupLogMsg(copymsg, const_cast<String*>(&name_));
+		
+
+		for (auto& sink : sinks_)
+		{
+			if (sink->should_log(copymsg.level))
+			{
+				try
+				{
+					sink->log(copymsg);
+				}
+
+				SPDLOG_CATCH_AND_HANDLE
+			}
+		}
+
+		for (auto& sink : sinks_)
+		{
+			try
+			{
+				sink->flush();
+			}
+
+			SPDLOG_CATCH_AND_HANDLE
+		}
+	}
+}
+
+Void AsyncLog::sink_it_(spdlog::details::log_msg& msg)
+{
+	try
+	{
+		msgQue_.push(std::move(AsyncLogMsg(msg)));
+	}
+	catch (const std::exception& e)
+	{
+		default_err_handler_(e.what());
+	}
+	catch (...)
+	{
+		default_err_handler_("Unknown Exception in AsyncLog" + name_);
+		throw;
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// class Logger
+
+Logger::~Logger()
+{
+	stop_ = true;
+	thread_.join();
+}
+
+Bool Logger::Setup(const String& filePath)
+{
+	auto fullPath = std::experimental::filesystem::path(filePath);
+	if (fullPath.empty())
+	{
+		return false;
+	}
+	
+	auto fileName = fullPath.filename().string();
+	fileName = fileName.substr(0, fileName.find_last_of('.'));
+
+	auto path = fullPath.parent_path();
+	
+	try
+	{
+		path += "\\Logs";
+		std::experimental::filesystem::create_directory(path);
+		path += ("\\" + fileName);
+		std::experimental::filesystem::create_directory(path);
+		path += "\\";
+	}
+	catch (std::exception& e)
+	{
+		e.what();
+		return false;
+	}
+
+	auto stdoutSink = std::make_shared<spdlog::sinks::wincolor_stdout_sink_st>();
+	
+	auto saveLogFilePath = path.string() + fileName + ".log";
+	auto rotatingSink = std::make_shared<spdlog::sinks::rotating_file_sink_st>(saveLogFilePath, LOG_FILE_SIZE, MAX_LOG_FILE_COUNT);
+
+	stdoutSink->set_level(spdlog::level::trace);
+	rotatingSink->set_level(spdlog::level::trace);
+
+	auto logger = std::make_shared<AsyncLog>("AsyncLogger", spdlog::sinks_init_list{ stdoutSink, rotatingSink });
+	logger->set_level(spdlog::level::trace);
+	logger->set_pattern("%^[%Y-%m-%d %H:%M:%S][%l][tid:%t][%s,%#] %v%$");
+
+	logger_ = logger;
+	thread_ = std::thread(&Logger::Flush, this);
+
+	return true;
+}
+
+Void Logger::Flush()
+{
+	while (false == stop_)
+	{
+		logger_->flush();
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+}
