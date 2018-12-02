1f08a94a37e7b9a9e7d137fe288874425552f975
Mk <ultraman232@naver.com>
2018-12-02 15:49:26 +0900

    DB 연결 쿼리 실행 테스트 코드 추가

--- TestServer/TestServer.cpp
+++ TestServer/TestServer.cpp
@@ -3,7 +3,56 @@
 
 #include "stdafx.h"
 #include "playerInfo_generated.h"
+#include "QueryTest.h"
 
+
+class DBThread : public Thread
+{
+
+	DBConnection*	connection_;
+	
+public:
+
+	static Short	GetDepartmentNumber()
+	{
+		return DB_PROCESSING_DEPT;
+	}
+
+	DBThread(DWORD id, Iocp* iocp, const String& name)
+		: Thread(id, name, DB_PROCESSING_DEPT)
+		, connection_(nullptr)
+	{
+
+	}
+
+	Bool Setup() final
+	{
+		if (false == __super::Setup())
+		{
+			return false;
+		}
+		
+		connection_ = new DBConnection();
+		if (nullptr == connection_)
+		{
+			return false;
+		}
+
+		connection_->Connect(Singleton<DBEnv>::GetInstance().GetEnv());
+		return true;
+	}
+
+	Void Run() final
+	{
+
+	}
+
+	Void ShutDown() final
+	{
+
+	}
+
+};
 
 class TestServer : public Server
 {
@@ -26,9 +75,11 @@ public:
 			return false;
 		}
 
-		GThreadManager.AddDepartment(NetworkIoThread::GetDepartmentNumber(), &iocp_);
-		GThreadManager.Start<NetworkIoThread>(0, "NetworkIoThread", &iocp_);
+		GThreadManager.AddDepartment(NETWORK_IO_PROCESSING_DEPT, &iocp_);
+		GThreadManager.Start<NetworkIoThread>(0, "NetworkIoThread");
 		
+		//GThreadManager.AddDepartment(DB_PROCESSING_DEPT, nullptr);
+		//GThreadManager.Start<DBThread>(5, "DBThread");
 
 		return true;
 	}
@@ -65,8 +116,8 @@ private:
 		Int level = playerdata->level();
 		auto name = playerdata->name()->c_str();
 		wchar_t name_[20] = { 0, };
-		convertCharToWide(const_cast<char*>(name), name_, strlen(name));
-		LOG_INFO(L"[Server] recv data :: name : {0}, level : {1}", name_, level);
+		ConvertCharToWide(const_cast<char*>(name), name_, strlen(name));
+		LOG_INFO(L"[Server] recv data :: name : {0}, level : {1} ", name_, level);
 		
 		MksBuilder echoBuilder;
 		auto echoData = CreatePlayerInfo(echoBuilder.Get(), echoBuilder.Get().CreateString(playerdata->name()), playerdata->level());
@@ -85,13 +136,18 @@ int main(int argc, CHAR* argv[])
 	
 	GLogger.Setup(argv[0]);
 		
-	LOG_INFO(L"..........Test Server Start..........");
-	LOG_INFO(L"Start Test Logging...");
+	//LOG_INFO(L"..........Test Server Start..........");
+	//LOG_INFO(L"Start Test Logging...");
 
 	/////////// DB Connection Test////////////////////////
 	// 연결이 정상적으로 이루어진다. 우선 연결만 확인.
 	DBConnection con;
 	con.Connect(Singleton<DBEnv>::GetInstance().GetEnv());
+	WString oldId(L"ultraman");
+	WString newId(L"mhkmbj232");
+	   
+	TestStringQuery query(oldId, newId);
+	query.Execute(&con);
 	//////////////////////////////////////////////////////
 	
 	std::shared_ptr<TestServer> server = std::make_shared<TestServer>(new TestPacketDispathcer);
