a4985006399f93d55e15fc9d1452792338532d5f
Mk <ultraman232@naver.com>
2018-12-02 15:52:31 +0900

    DBQuery, DBRecord 파일 추가 DBQuery는 테스트 성공

---
+++ FrameWork/DBQuery.cpp
@@ -0,0 +1,108 @@
+#include "stdafx.h"
+
+DBQuery::DBQuery()
+	: queryStatement_(L"")
+{
+
+}
+
+DBQuery::DBQuery(const WString& queryStatement)
+	: queryStatement_(queryStatement)
+{
+
+}
+
+DBQuery::~DBQuery()
+{
+	if (hstmt_ != SQL_NULL_HSTMT)
+	{
+		SQLRETURN ret = ::SQLCloseCursor(hstmt_);
+		try
+		{
+			ret = ::SQLFreeStmt(hstmt_, SQL_UNBIND);
+			ret = ::SQLFreeStmt(hstmt_, SQL_RESET_PARAMS);
+			ret = ::SQLFreeStmt(hstmt_, SQL_CLOSE);
+		}
+		catch (std::exception& e)
+		{
+			LOG_ERROR(L"SQLFreeStmt{0}", e.what());
+		}
+	}
+
+	queryStatement_.clear();
+	params_.clear();
+}
+
+
+
+Void		DBQuery::SetQuery(const WString& queryStatement)
+{
+	queryStatement_ = queryStatement;
+}
+
+WString		DBQuery::GetQuery()
+{
+	return queryStatement_;
+}
+
+Bool		DBQuery::Execute(DBConnection* const connection)
+{
+	if (nullptr == connection)
+	{
+		return false;
+	}
+
+	hstmt_ = connection->GetHstmt();
+	
+	if (false == preparingParams(connection))
+	{
+		LOG_ERROR(L"DBQuery Execute => bindParams Failed.");
+		return false;
+	}
+
+	// query execute
+	SQLRETURN ret = ::SQLExecDirectW(hstmt_, const_cast<WChar*>(queryStatement_.c_str()), SQL_NTSL);
+	if (ret != SQL_NO_DATA && false == DBErrorMessage::CheckError(connection, SQL_HANDLE_STMT, hstmt_, ret))
+	{
+		LOG_ERROR(L"Query Execute Failed. Query:{0}", queryStatement_.c_str());
+		return false;
+	}
+	
+	// fetchColumnData
+	//  getColumnData()
+
+	// hstmt_ Á¤¸®
+	// resetStmt;
+
+
+	return true;
+}
+
+Bool		DBQuery::preparingParams(DBConnection* const connection)
+{
+	if (params_.size() <= 0)
+	{
+		return false;
+	}
+
+	if (hstmt_ == NULL)
+	{
+		return false;
+	}
+
+	SQLUSMALLINT index = 1;
+	for (auto& p : params_)
+	{
+		std::wcout << *(static_cast<wchar_t*>(p.paramValuePtr_)+1) << std::endl;
+
+		auto ret = ::SQLBindParameter(hstmt_, index++, p.ioType_, p.cType_, p.sType_, p.columnSize_,
+			p.decimalDigits_, p.paramValuePtr_, p.paramValueSize_, &p.strLen_or_lnd_);
+
+		if (false == DBErrorMessage::CheckError(connection, SQL_HANDLE_STMT, hstmt_, ret))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ FrameWork/DBQuery.h
@@ -0,0 +1,85 @@
+#pragma once
+
+struct ParamData
+{
+	SQLUSMALLINT	paramOrder_;			//  (?,?,?) param 순서 (1) 부터시작
+	SQLSMALLINT		ioType_;				//	input, output
+	SQLSMALLINT		cType_;					//	C 언어에서 사용하는 데이터 타입
+	SQLSMALLINT		sType_;					//	SQL 데이터 타입
+	SQLUINTEGER		columnSize_;			//  파라미터의 데이터 사이즈 지정
+	SQLSMALLINT		decimalDigits_;			//  바이너리 데이터에서는 사용 않음,
+	SQLPOINTER		paramValuePtr_;			//  데이터의 포인터
+	SQLLEN			paramValueSize_;		//  데이터의 크기
+	SQLLEN			strLen_or_lnd_;			//  SQL_DATA_AT_EXEC 입력, SQLPutData() 함수를 통해서 데이터를 보낸다고 알려줌.
+											//  문자열사용시 SQL_NTS
+
+	ParamData(SQLSMALLINT sqlType, SQLSMALLINT cType, SQLSMALLINT ioType_)
+		: paramOrder_(0)
+		, ioType_(ioType_)
+		, cType_(cType)
+		, sType_(sqlType)
+		, columnSize_(0)
+		, decimalDigits_(0)
+		, paramValuePtr_(nullptr)
+		, paramValueSize_(0)
+		, strLen_or_lnd_(0)
+	{
+	}
+};
+
+
+class DBQuery
+{
+
+	using ParamDatas = std::vector<ParamData>;
+	
+	WString			queryStatement_;
+	SQLHSTMT		hstmt_;
+	ParamDatas		params_;
+
+public:
+
+	DBQuery();
+	explicit DBQuery(const WString& queryStatement);
+	virtual ~DBQuery();
+
+	template <typename Arg, typename... Args>
+	Void BindParams(Arg&& arg, Args&&... args)
+	{
+		bind(std::forward<Arg>(arg));
+		BindParams(std::forward<Args>(args)...);
+	}
+
+	Void BindParams()
+	{
+	}
+
+	Bool		Execute( DBConnection* const connection);
+
+private:
+
+	template<typename T>
+	Void bind(T*&& value)
+	{
+		auto[sqltype, ctype] = SqlTypeHelper::GetType<T>();
+		ParamData param(sqltype, ctype, SQL_PARAM_INPUT);
+		param.paramValuePtr_ = value;
+		param.paramValueSize_ = sizeof(*value);
+
+		params_.emplace_back(param);
+	}
+
+	template<>
+	void bind(const wchar_t*&& val)
+	{
+		ParamData param(SQL_WVARCHAR, SQL_C_WCHAR, SQL_PARAM_INPUT);
+		param.paramValuePtr_ = const_cast<wchar_t*>(val);
+		param.paramValueSize_ = (::wcslen(val) + 1) * sizeof(wchar_t);
+		param.strLen_or_lnd_ = SQL_NTSL;
+
+		params_.emplace_back(param);
+	}
+
+
+	Bool		preparingParams(DBConnection* const connection);
+};
